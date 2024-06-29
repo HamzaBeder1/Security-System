901fb622c78afef8f4f9462db7d5262b1f3d37b9
HamzaBeder1 <hamzabeder0@gmail.com>
2024-06-29 04:48:07 -0500

    Added opencv code.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
-import cv2
+import cv2 as cv
 from picamera2 import Picamera2, Preview
 from picamera2.encoders import H264Encoder
 
@@ -20,7 +20,7 @@ class Camera():
     def __init__(self):
         self.picam2 = Picamera2()
     
-    def take_video(self):
+    def take_video2(self):
         video_config = self.picam2.create_video_configuration()
         self.picam2.configure(video_config)
         
@@ -29,6 +29,25 @@ class Camera():
         self.picam2.start_recording(encoder, 'test.h264')
         sleep(5)
         self.picam2.stop_recording()
+    
+    """ def take_video(self):
+        if not self.v.isOpened():
+            print("Cannot open camera")
+            exit()
+        try:
+            x, frame = self.v.read()
+            #if not x:
+               # raise ValueError('cannot receive frame')
+            cv.imshow('test', frame)
+        except:
+            exit()
+         """
+         
+    def take_video(self):
+        capture = cv.VideoCapture('test.h264')
+        while True:
+            isTrue, frame = capture.read()
+            cv.imshow('Video', frame)
         
         
 def setup():
